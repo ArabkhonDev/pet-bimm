386bba8f58c8de00b7b4a25a58191953b80a82f3
ArabkhonDev <starc9899@gmail.com>
2024-05-17 23:08:33 +0500

    change file

--- src/components/raxbariyat/Test.jsx
+++ src/components/raxbariyat/Test.jsx
@@ -5,10 +5,12 @@ export const Test = ({h}) => {
     const [count, setCount] = useState(1);
     return (
       <ul>
-        <button onClick={() => setCount(1)}>work</button>
-        <button onClick={() => setCount(2)}>Info</button>
-        {count === 1 && <p>Work</p>}
-        {count === 2 && <p>info</p>}
+        <button onClick={() => setCount(1)}>Info</button>
+        <button onClick={() => setCount(2)}>work</button>
+        <button onClick={() => setCount(3)}>Task</button>
+        {count === 1 && <p>Info</p>}
+        {count === 2 && <p>Work</p>}
+        {count === 3 && <p>info</p>}
       </ul>
   );
 };
