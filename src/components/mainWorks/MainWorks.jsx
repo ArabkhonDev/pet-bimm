07cfc1ae9f3e2c5e4fdb5c72877f06dc47424586
ilosrim <ilosrim@yahoo.com>
2024-05-27 11:17:34 -0700

    edited

--- src/components/mainWorks/MainWorks.jsx
+++ src/components/mainWorks/MainWorks.jsx
@@ -2,59 +2,65 @@ import { getData } from "../../data/mainWorks";
 import "./mainWorks.css";
 
 const MainWorks = () => {
-  const catalogs = getData();
-  return (
-    <div className="main_works">
-      <div className="container">
-        <div className="main">
-          <div className="main_works_info">
-            <div className="main_works_title">
-              <h2>BOSH ILMIY-METODIK MARKAZ</h2>
-            </div>
-            <ol>
-              <li>
-                <p>
-                  .......﻿O‘zbekiston Respublikasi Oliy ta’lim, fan va
-                  innovatsiyalar vazirligi huzuridagi Oliy ta`lim tizimi pedagog
-                  va rahbar kadrlarini qayta tayyorlash va ularning malakasini
-                  oshirishni tashkil etish Bosh ilmiy-metodik markazi
-                  O`zbekiston Respublikasi Vazirlar Mahkamasining 2012 yil 26
-                  sentyabrdagi "Oliy ta`lim muassasalari pedagog kadrlarini
-                  qayta tayyorlash va ularning malakasini oshirish tizimini
-                  yanada takomillashtirish chora-tadbirlari to`g`risida"gi
-                  278-son qarori bilan tashkil etilg
-                </p>
-              </li>
-              <li>
-                <p>
-                  Bosh ilmiy-metodik markazning maqsadi - oliy ta`lim tizimi
-                  pedagog va rahbar kadrlarini qayta tayyorlash va malakasini
-                  oshirish jarayonlari sifatini takomillashtirish va
-                  monitoringini olib borish, ularni me`yoriy va metodik
-                  ta`minotini amalga oshirish, qayta tayyorlash va malaka
-                  oshirish jarayonlariga zamonaviy pedagogik va
-                  axborot-kommunikatsiya texnologiyalarini keng joriy etilishini
-                  tashkillashtirishdan iborat.
-                </p>
-              </li>
-            </ol>
-          </div>
+	const catalogs = getData();
+	return (
+		<div className="main_works">
+			<div className="container">
+				<div className="main">
+					<div className="main_works_info">
+						<div className="main_works_title">
+							<h2>BOSH ILMIY-METODIK MARKAZ</h2>
+						</div>
+						<ol>
+							<li>
+								<p>
+									.......O‘zbekiston Respublikasi Oliy ta’lim, fan va
+									innovatsiyalar vazirligi huzuridagi Oliy ta`lim tizimi pedagog
+									va rahbar kadrlarini qayta tayyorlash va ularning malakasini
+									oshirishni tashkil etish Bosh ilmiy-metodik markazi
+									O`zbekiston Respublikasi Vazirlar Mahkamasining 2012 yil 26
+									sentyabrdagi "Oliy ta`lim muassasalari pedagog kadrlarini
+									qayta tayyorlash va ularning malakasini oshirish tizimini
+									yanada takomillashtirish chora-tadbirlari to`g`risida"gi
+									278-son qarori bilan tashkil etilg
+								</p>
+							</li>
+							<li>
+								<p>
+									Bosh ilmiy-metodik markazning maqsadi - oliy ta`lim tizimi
+									pedagog va rahbar kadrlarini qayta tayyorlash va malakasini
+									oshirish jarayonlari sifatini takomillashtirish va
+									monitoringini olib borish, ularni me`yoriy va metodik
+									ta`minotini amalga oshirish, qayta tayyorlash va malaka
+									oshirish jarayonlariga zamonaviy pedagogik va
+									axborot-kommunikatsiya texnologiyalarini keng joriy etilishini
+									tashkillashtirishdan iborat.
+								</p>
+							</li>
+						</ol>
+					</div>
 
-          <div className="main_works_catalog">
-            {catalogs.map((cat) => (
-              <li className="main_works_catalog_item" key={cat.id}>
-                <img src={cat.Image} alt={cat.title} />
-                <p>{cat.title}</p>
-                <button className="main_works_catalog_item_btn">
-                  Batafsil
-                </button>
-              </li>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className="main_works_catalog">
+						{catalogs.map((cat) => (
+							<li
+								className="main_works_catalog_item"
+								key={cat.id}
+							>
+								<img
+									src={cat.Image}
+									alt={cat.title}
+								/>
+								<p>{cat.title}</p>
+								<button className="main_works_catalog_item_btn">
+									Batafsil
+								</button>
+							</li>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MainWorks;
--- src/components/raxbariyat/Test.jsx
+++ src/components/raxbariyat/Test.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
+import { Info } from "./Info";
+import { Work } from "./Work";
+import { Task } from "./Task";
 
-
-export const Test = ({h}) => {
-    const [count, setCount] = useState(1);
-    return (
-      <ul>
-        <button onClick={() => setCount(1)}>Info</button>
-        <button onClick={() => setCount(2)}>work</button>
-        <button onClick={() => setCount(3)}>Task</button>
-        {count === 1 && <p>Info</p>}
-        {count === 2 && <p>Work</p>}
-        {count === 3 && <p>info</p>}
-      </ul>
-  );
+export const Test = ({ h }) => {
+	const [count, setCount] = useState(1);
+	return (
+		<ul>
+			<button onClick={() => setCount(1)}>Info</button>
+			<button onClick={() => setCount(2)}>work</button>
+			<button onClick={() => setCount(3)}>Task</button>
+			{count === 1 && <Info />}
+			{count === 2 && <Work />}
+			{count === 3 && <Task />}
+		</ul>
+	);
 };
